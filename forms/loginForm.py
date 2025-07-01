35ffc93d196ee44b43cdba45b2abde666015fc0d
Ellie <ellie.o_2023_lon@outlook.com>
2025-07-01 19:19:10 +0100

    E-track creation

--- forms/loginForm.py
+++ forms/loginForm.py
@@ -7,4 +7,4 @@ from wtforms.validators import InputRequired, length
 class loginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired('username required'), length(min=5, max=50, message='Username must be in 5 to 50 characters')])
     password = PasswordField('Password',validators=[InputRequired('Password required'), length(min=8)])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Login')
