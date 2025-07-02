b833312a8b8f48e94214b19e63fdabf1a22f757f
Ellie <ellie.o_2023_lon@outlook.com>
2025-07-02 16:03:42 +0100

    Adding port binding

--- app.py
+++ app.py
@@ -44,3 +44,6 @@ def create_app(config=None):
     return app
 
 app = create_app()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
